42ef96cdd40255ac1b8942e3c46b4efeb6905032
Espen Berget <espen.berget@tuta.io>
2021-02-21 13:37:48 +0100

    added back and skip buttons

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const display = document.getElementById("display");
 const speed = document.getElementById("speed");
 const play = document.getElementById("playPause");
+const back = document.getElementById("back");
+const skip = document.getElementById("skip");
 const fileDisplay = document.getElementById("fileDisplay");
 const fileSelect = document.getElementById("fileSelect");
 
@@ -67,11 +69,10 @@ function nextWord() {
 }
 
 
-// Reader
 let req; // holds the current request frame, used to start/cancel animation
 let page; // holds a parsed version off the file. used to display and progress the reader
 
-let speedValue = 500;
+let speedValue = 500; // the speed of the reader
 
 // Dynamically change reading speed
 speed.oninput = ev => {
@@ -80,11 +81,48 @@ speed.oninput = ev => {
 
 // STOP/START on button press and when user presses space
 play.onclick = togglePlay;
+back.onclick = moveBack;
+skip.onclick = moveForward;
 window.addEventListener("keypress", ev => {
-    if (ev.key === " ") {
+    if (ev.key === " " || ev.key === "k") {
         togglePlay();
+    } else if (ev.key === "j") {
+        moveBack();
+    } else if (ev.key === "l") {
+        moveForward();
     }
 })
+
+function moveBack() {
+    const diff = page.wordIx - 5;
+    if (diff >= 0) {
+        page.wordIx -= 5;
+    } else if (diff < 0 && page.ix !== 0) {
+        page.ix--;
+        page.wordIx = page.sentences[page.ix].length + diff;
+    } else {
+        page.wordIx = 0;
+    }
+
+    fileDisplay.innerHTML = view(page);
+}
+
+function moveForward() {
+    const diff = (page.sentences[page.ix].length-1) - page.wordIx;
+    if (diff >= 5) {
+        page.wordIx += 5;
+        console.log("first");
+    } else if (diff < 5 && page.ix !== page.sentences.length-1) {
+        page.ix++;
+        page.wordIx = (5 - diff) - 1;
+        console.log("second");
+    } else {
+        page.wordIx = page.sentences[page.ix].length-1;
+        console.log("third");
+    }
+
+    fileDisplay.innerHTML = view(page);
+}
 
 function toggleDisplay() {
     if (play.textContent === "play") {
@@ -95,6 +133,7 @@ function toggleDisplay() {
 }
 
 function togglePlay() {
+    if (page === undefined) return;
     if (play.textContent === "pause") {
         window.cancelAnimationFrame(req);
     } else {
@@ -103,6 +142,8 @@ function togglePlay() {
     toggleDisplay();
 }
 
+
+// Animation
 let start;
 function step(timestamp) {
     req = window.requestAnimationFrame(step);
